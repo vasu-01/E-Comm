357702b79345a132cbc3fdf7b703633d195e3574
Vishal Kumar <vishalyadav6jd@gmail.com>
2025-09-22 23:51:48 +0530

    first commit

---
+++ frontend-Demo/src/assets/image.js
@@ -0,0 +1,25 @@
+import EcommIcon from "./images/icon.png";
+import Paypal from "./images/paypal.png";
+import WesternUnion from "./images/western-union.png";
+import MasterCard from "./images/group 19.png";
+import Shoe_1 from "./images/banner-shoe.png";
+import menuSwitch from "./images/menuSwitch.png";
+import expandSwitch from "./images/expandSwitch.png";
+import Shoe_2 from "./images/shoe_2.png";
+import Shoe_3 from "./images/shoe_3.png";
+import Shoe_4 from "./images/shoe_4.png";
+import Shoe_5 from "./images/shoe_5.png";
+
+export {
+  EcommIcon,
+  Paypal,
+  WesternUnion,
+  MasterCard,
+  Shoe_1,
+  menuSwitch,
+  expandSwitch,
+  Shoe_2,
+  Shoe_3,
+  Shoe_4,
+  Shoe_5,
+};
